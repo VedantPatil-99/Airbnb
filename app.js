65d329d77c3b7b2cce5b81ad1a17b5173cb7058c
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-01-12 00:14:27 +0530

    Error Handle: ExpressError & wrapAsync (Folder: Utils)
    Created ExpressError class to handle custom errors, and a wrapAsync function also. All async functions to certain routes are enclosed now in wrapAsync fn.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const Listing = require("./models/listings");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -33,10 +35,13 @@ app.get("/", (req, res) => {
 });
 
 // Index Route
-app.get("/listings", async (req, res) => {
-	let allListings = await Listing.find({});
-	res.render("./listings/index.ejs", { allListings });
-});
+app.get(
+	"/listings",
+	wrapAsync(async (req, res) => {
+		let allListings = await Listing.find({});
+		res.render("./listings/index.ejs", { allListings });
+	}),
+);
 
 // New Route
 app.get("/listings/new", (req, res) => {
@@ -44,39 +49,67 @@ app.get("/listings/new", (req, res) => {
 });
 
 // Show Route
-app.get("/listings/:id", async (req, res) => {
-	let { id } = req.params;
-	let listing = await Listing.findById(id);
-	res.render("./listings/show.ejs", { listing });
-});
+app.get(
+	"/listings/:id",
+	wrapAsync(async (req, res) => {
+		let { id } = req.params;
+		let listing = await Listing.findById(id);
+		res.render("./listings/show.ejs", { listing });
+	}),
+);
 
 // Create Route
-app.post("/listings", async (req, res) => {
-	let newListing = new Listing(req.body.listing);
-	await newListing.save();
-	res.redirect("/listings");
-});
+app.post(
+	"/listings",
+	wrapAsync(async (req, res) => {
+		let newListing = new Listing(req.body.listing);
+		await newListing.save();
+		res.redirect("/listings");
+	}),
+);
 
 // Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
-	let { id } = req.params;
-	let listing = await Listing.findById(id);
-	res.render("./listings/edit.ejs", { listing });
-});
+app.get(
+	"/listings/:id/edit",
+	wrapAsync(async (req, res) => {
+		let { id } = req.params;
+		let listing = await Listing.findById(id);
+		res.render("./listings/edit.ejs", { listing });
+	}),
+);
 
 // Update Route
-app.put("/listings/:id", async (req, res) => {
-	let { id } = req.params;
-	// console.log(req.body.listing);
-	await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-	res.redirect(`/listings/${id}`);
-});
+app.put(
+	"/listings/:id",
+	wrapAsync(async (req, res) => {
+		let { id } = req.params;
+		// console.log(req.body.listing);
+		await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+		res.redirect(`/listings/${id}`);
+	}),
+);
 
 // Delete Route
-app.delete("/listings/:id", async (req, res) => {
-	let { id } = req.params;
-	let deletedListing = await Listing.findByIdAndDelete(id);
-	res.redirect("/listings");
+app.delete(
+	"/listings/:id",
+	wrapAsync(async (req, res) => {
+		let { id } = req.params;
+		let deletedListing = await Listing.findByIdAndDelete(id);
+		res.redirect("/listings");
+	}),
+);
+
+app.all("*", (req, res, next) => {
+	next(new ExpressError(404, "Page Not Found!"));
+});
+
+app.use((err, req, res, next) => {
+	let { statusCode = 500, message = "Something Went Wrong!" } = err;
+	res.status(statusCode).send(message);
+});
+
+app.listen(8080, (res) => {
+	console.log("Server is listening to the port 8080");
 });
 
 // app.get("/testListing", async (req, res) => {
@@ -92,7 +125,3 @@ app.delete("/listings/:id", async (req, res) => {
 //   res.send("Successful");
 //   console.log("Sample was saved");
 // })
-
-app.listen(8080, (res) => {
-	console.log("Server is listening to the port 8080");
-});
