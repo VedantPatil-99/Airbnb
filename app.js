f92f87b44ebe6423d590e2d400628d537a76808a
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-01-13 13:19:03 +0530

    Authentication using passport-local for users
    User schema is defined in /models/user.js; using passport, passport-local, passport-local-mongoose packages there will be user authorization & authentication. Minor: rename required routers in app.js

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ let app = express();
 
 const mongoose = require("mongoose");
 const path = require("path");
-const methodOverride = require("method-override");
-const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+// npm packages
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
 const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+
+// Routers
+const listingsRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/review.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -19,17 +25,6 @@ app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
-
-const sessionOptions = {
-	secret: "mysupersecretcode",
-	resave: false,
-	saveUninitialized: true,
-	cookie: {
-		expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-		maxAge: 7 * 24 * 60 * 60 * 1000,
-		httpOnly: true,
-	},
-};
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/airbnb";
 
@@ -49,9 +44,28 @@ app.get("/", (req, res) => {
 	res.send("Hi, I'm root.");
 });
 
+const sessionOptions = {
+	secret: "mysupersecretcode",
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+		maxAge: 7 * 24 * 60 * 60 * 1000,
+		httpOnly: true,
+	},
+};
+
 app.use(cookieParser());
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
 	res.locals.success = req.flash("success");
@@ -59,8 +73,18 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.get("/demouser", async (req, res) => {
+	let fakeUser = {
+		email: "abc@gmail.com",
+		username: "demo",
+	};
+
+	let regUser = await User.register(fakeUser, "password");
+	res.send(regUser);
+});
+
+app.use("/listings", listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
 
 app.all("*", (req, res, next) => {
 	next(new ExpressError(404, "Page Not Found!"));
