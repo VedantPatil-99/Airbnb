6a7973b23ae54d31ce74e2c0514b23b207b32eea
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-01-14 01:20:51 +0530

    Authorization for /reviews
    Middleware: isReviewAuthor prevents deletion of the reviews from different authors.
    Schema change for models/review.js (author prop added).
    To use authors details, need to populate the authors, so changes are made in routes/listing.js
    isReviewAuthor middleware is given to the delete review route.
    In show.ejs username is shown for each review.

--- routes/review.js
+++ routes/review.js
@@ -3,17 +3,22 @@ const router = express.Router({ mergeParams: true });
 const Listing = require("../models/listings");
 const Review = require("../models/review");
 const wrapAsync = require("../utils/wrapAsync.js");
-const { validateReview } = require("../middleware.js");
+const {
+	validateReview,
+	isLoggedIn,
+	isReviewAuthor,
+} = require("../middleware.js");
 
 // REVIEWS
 // Post Review Route
 router.post(
 	"/",
+	isLoggedIn,
 	validateReview,
 	wrapAsync(async (req, res) => {
 		let listing = await Listing.findById(req.params.id);
 		let newReview = new Review(req.body.review);
-
+		newReview.author = req.user._id;
 		listing.reviews.push(newReview);
 		await newReview.save();
 		await listing.save();
@@ -25,6 +30,7 @@ router.post(
 // Delete Review Route
 router.delete(
 	"/:reviewId",
+	isReviewAuthor,
 	wrapAsync(async (req, res) => {
 		let { id, reviewId } = req.params;
 
