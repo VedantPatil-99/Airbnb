ec41e2e123aaf710c65f7b486ea42dd2d6cbb7dc
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-01-15 20:10:20 +0530

    Map Functionality
    Using Mapbox GL JS API to show geo-location of the listings location.
    Now it is just implemented on the webpage.
    Its JS added in script.js and map div is in views/listings/show.ejs

--- public/js/script.js
+++ public/js/script.js
@@ -29,3 +29,11 @@ document.addEventListener("DOMContentLoaded", () => {
 		toast.show();
 	}
 });
+
+console.log(mapToken);
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+	container: "map", // container ID
+	center: [-74.5, 40], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+	zoom: 9, // starting zoom
+});
