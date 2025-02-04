dd4d354970f0f55776b82dfb3241d246662892db
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-02-05 00:33:20 +0530

    Refactor display of rating stars

--- public/js/showRatings.js
+++ public/js/showRatings.js
@@ -1,10 +1,15 @@
-let rated = document.querySelectorAll(".s-rated");
-console.log(rated);
-rated.forEach((rate, idx) => {
-	console.log(rate.textContent);
-	if (rate.textContent) {
-		rate.innerHTML = svgData.smallStar;
-	} else {
-		console.log("wddkkd");
-	}
+document.addEventListener("DOMContentLoaded", function () {
+	let rated = document.querySelectorAll(".s-rated");
+	// console.log("First Review Rating:", listing.reviews[0]?.rating);
+
+	rated.forEach((rate, idx) => {
+		if (listing.reviews[idx]) {
+			let rating = listing.reviews[idx].rating;
+			// console.log(`Review ${idx + 1} Rating:`, rating);
+
+			if (!isNaN(rating) && rating > 0) {
+				rate.innerHTML = svgData.smallStar.repeat(rating);
+			}
+		}
+	});
 });
