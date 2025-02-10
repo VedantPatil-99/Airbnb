2715c3acd3859c9ee4a5e5c5af4710df92668ca8
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-02-11 00:02:48 +0530

    Add review modal content

--- public/js/script.js
+++ public/js/script.js
@@ -29,3 +29,13 @@ document.addEventListener("DOMContentLoaded", () => {
 		toast.show();
 	}
 });
+
+const popoverTriggerList = document.querySelectorAll(
+	'[data-bs-toggle="popover"]',
+);
+const popoverList = [...popoverTriggerList].map(
+	(popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl),
+);
+const popover = new bootstrap.Popover(".popover-dismiss", {
+	trigger: "focus",
+});
